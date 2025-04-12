d686115ee738d8803b0f4d27206d6a16c4b89b60
NoFun4U-04 <ducthinh4129@gmail.com>
2025-04-12 12:50:58 +0000

    Updated

--- scripts/prompt_templates.py
+++ scripts/prompt_templates.py
@@ -1,11 +1,8 @@
 quadruplet_instruction = """Hãy xác định loại khía cạnh, cụm từ thể hiện khía cạnh, cụm từ thể hiện ý kiến và trạng thái ý kiến (tốt tạm, tệ) cho bình luận sau đây: \"{}\"\nTrả lời:"""
 
-def get_prompt(input_review, prompt_format, task):
+def get_prompt(input_review, task):
     if task == 'quadruplet':
         instruction = quadruplet_instruction.format(input_review)
     
-    if prompt:
-        prompt = prompt.format(instruction.format(input_review))
-    else:
-        prompt = instruction.format(input_review)
+    prompt = instruction.format(input_review)
     return prompt
